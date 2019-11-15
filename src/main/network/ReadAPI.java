a3bdc06e5f3aa33d136c0345abd6fa876f105639
Brian Yeung <brianyeung@live.com>
2019-11-14 22:34:02 -0800

    parse JSON

--- src/main/network/ReadAPI.java
+++ src/main/network/ReadAPI.java
@@ -1,7 +1,5 @@
 package network;
 
-import com.sun.deploy.util.StringUtils;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,12 +16,10 @@ public class ReadAPI {
         try {
             String apikey = "4152dedf986c5f6708ae726c5d148aa5";
             String londonweatherquery = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=";
-            String theURL = londonweatherquery+apikey;
-            URL url = new URL(theURL);
+            URL url = new URL(londonweatherquery + apikey);
             br = new BufferedReader(new InputStreamReader(url.openStream()));
 
             String line;
-
             StringBuilder sb = new StringBuilder();
 
             while ((line = br.readLine()) != null) {
@@ -31,15 +27,7 @@ public class ReadAPI {
                 sb.append(System.lineSeparator());
             }
 
-            System.out.println(sb);
-            String s = sb.toString();
-            int min = s.indexOf("temp_min") + 10;
-            int max = s.indexOf("temp_max") + 10;
-            Float mintemp = Float.parseFloat(s.substring(min, min+6)) - 273;
-            Float maxtemp = Float.parseFloat(s.substring(max, max+6)) - 273;
-
-            System.out.println("London has high of " + maxtemp + " and low of " + mintemp + ".");
-
+            printHighLow(sb);
 
         } finally {
 
@@ -47,5 +35,19 @@ public class ReadAPI {
                 br.close();
             }
         }
+    }
+
+    private static void printHighLow(StringBuilder sb) {
+        String s = sb.toString();
+        printHighAndLow(s);
+    }
+
+    private static void printHighAndLow(String s) {
+        int min = s.indexOf("temp_min") + 10;
+        int max = s.indexOf("temp_max") + 10;
+        Float mintemp = Float.parseFloat(s.substring(min, min + 6)) - 273;
+        Float maxtemp = Float.parseFloat(s.substring(max, max + 6)) - 273;
+
+        System.out.println("London has high of " + maxtemp + " and low of " + mintemp + ".");
     }
 }
